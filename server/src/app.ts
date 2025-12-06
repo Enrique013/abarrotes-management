72b252db021c2848a32b7f5dd1547956b8aeb49b
Isaac <jim.isaacmiranda@gmail.com>
2025-12-06 00:07:16 -0600

    new modules added

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import authRoutes from './modules/auth/auth.routes.js';
 import usersRoutes from './modules/users/users.routes.js';
 import storesRoutes from './modules/stores/stores.routes.js';
+import productsRoutes from './modules/products/products.routes.js';
+import visitsRoutes from './modules/visits/visits.routes.js';
+import ordersRoutes from './modules/orders/orders.routes.js';
 
 const app = express();
 
@@ -17,5 +20,8 @@ app.get('/', (_req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/stores', storesRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/visits', visitsRoutes);
+app.use('/api/orders', ordersRoutes);
 
 export default app;
